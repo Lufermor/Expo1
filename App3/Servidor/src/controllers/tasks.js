fdc575c9b367672b4ee583a6b79a36262c5fdca1
Lufermor <luisfergoza@gmai.com>
2023-03-22 05:39:35 +0100

    Configuración de rutas crud para las tareas

--- App3/Servidor/src/controllers/tasks.js
+++ App3/Servidor/src/controllers/tasks.js
@@ -20,7 +20,23 @@ export const getTasks = (req, res) => {
 
 export const getTaskCount = (req, res) => {
     // NOTA: falta restringir la búsqueda a las task del usuario logeado (req.user.id o algo así)
-    queryDatabase('SELECT * FROM tasks WHERE id = ', [req.params.id],
+    queryDatabase('SELECT COUNT(*) FROM tasks', null,
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send("Error interno del servidor al obtener datos de la base de datos");
+                return;
+            }
+            //Nos devuelve un json con clave COUNT(*), así que le solicitamos el valor de esa clave
+            res.status(200).send(JSON.stringify(results[0]["COUNT(*)"], null, 3));
+        }
+    );
+}
+
+export const getTask = (req, res) => {
+    // NOTA: falta restringir la búsqueda a las task del usuario logeado (req.user.id o algo así)
+    console.log(`id obtenida: ${req.params.id}`);
+    queryDatabase('SELECT * FROM tasks WHERE id = ?', [req.params.id],
         (err, results) => {
             if (err) {
                 console.error(err);
@@ -32,18 +48,45 @@ export const getTaskCount = (req, res) => {
     );
 }
 
-export const getTask = (req, res) => {
-    res.send("Hola mundo react desde server");
-}
-
 export const saveTask = (req, res) => {
-    res.send("Hola mundo react desde server");
+    // NOTA: falta restringir la búsqueda a las task del usuario logeado (req.user.id o algo así)
+    //${"usuario_prueba"}, ${'Circuitos'}, ${'Leer capítulo 1'}
+    //["usuario_prueba", 'Circuitos', 'Leer capítulo 1']
+    queryDatabase(`INSERT INTO tasks(usuario_id, title, description) VALUES (?,?,?)`, [req.body.usuario_id, req.body.title, req.body.description],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send("Error interno del servidor al obtener datos de la base de datos");
+                return;
+            }
+            res.status(200).send("Tarea creada exitosamente");
+        }
+    );
 }
 
 export const deleteTask = (req, res) => {
-    res.send("Hola mundo react desde server");
+    queryDatabase('DELETE FROM tasks WHERE id = ?', [req.params.id],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send("Error interno del servidor al obtener datos de la base de datos");
+                return;
+            }
+            res.status(204).send(JSON.stringify(results, null, 3));
+        }
+    );
 }
 
 export const updateTasks = (req, res) => {
-    res.send("Hola mundo react desde server");
+    queryDatabase(`UPDATE tasks SET ? WHERE id = ?`, [req.body, req.params.id],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send("Error interno del servidor al obtener datos de la base de datos");
+                return;
+            }
+            console.log(results);
+            res.status(204).send("Tarea actualizada exitosamente");
+        }
+    );
 }
--- App3/Servidor/src/routes/tasks.js
+++ App3/Servidor/src/routes/tasks.js
@@ -4,16 +4,65 @@ import {deleteTask, getTask, getTaskCount, getTasks, saveTask, updateTasks} from
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: tasks endpoint
+ */
+
+/**
+ * @swagger
+ * /tasks:
+ *  get:
+ *    summary: Obtiene todas las tareas
+ *    tags: [Tasks]
+ */
 router.get('/tasks', getTasks);
 
+/**
+ * @swagger
+ * /tasks/count:
+ *  get:
+ *    summary: Obtiene el total de tareas
+ *    tags: [Tasks]
+ */
 router.get('/tasks/count', getTaskCount);
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  get:
+ *    summary: Obtiene una tarea 
+ *    tags: [Tasks]
+ */
 router.get('/tasks/:id', getTask);
 
+/**
+ * @swagger
+ * /tasks:
+ *  post:
+ *    summary: Guarda una nueva tarea
+ *    tags: [Tasks]
+ */
 router.post('/tasks', saveTask);
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  delete:
+ *    summary: Borra una tarea de la que se pasa su id
+ *    tags: [Tasks]
+ */
 router.delete('/tasks/:id', deleteTask);
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  put:
+ *    summary: Modifica una tarea
+ *    tags: [Tasks]
+ */
 router.put('/tasks/:id', updateTasks);
 
 export default router;
